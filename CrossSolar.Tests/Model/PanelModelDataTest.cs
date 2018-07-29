bf05dc62e7ce7794a451834b4eeb5ff32efe4be0
elton naoki iwakura <naoki_iwakura@hotmail.com>
2018-07-29 17:14:52 -0400

    validate data for panelmodel

---
+++ CrossSolar.Tests/Model/PanelModelDataTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using CrossSolar.Controllers;
+using CrossSolar.Models;
+using CrossSolar.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace CrossSolar.Tests.Model
+{
+    public class PanelModelDataTest
+    {
+        [Fact]
+        public void PanelModelValidateData()
+        {
+            var panelModelNotValid = new PanelModel
+            {
+                Brand = "Areva",
+                Latitude = 91,
+                Longitude = 181,
+                Serial = "XXXX1111YYYY2222"
+            };
+
+            var context1 = new ValidationContext(panelModelNotValid, null, null);
+            var results1 = new List<ValidationResult>();
+            var isModelStateNotValid = Validator.TryValidateObject(panelModelNotValid, context1, results1, true);
+
+            var panelModelValid = new PanelModel
+            {
+                Brand = "Areva",
+                Latitude = 62.946264,
+                Longitude = 180,
+                Serial = "XXXX1111YYYY2222"
+            };
+
+            var context = new ValidationContext(panelModelValid, null, null);
+            var results = new List<ValidationResult>();
+            var isModelStateValid = Validator.TryValidateObject(panelModelValid, context, results, true);
+
+
+            // Assert 
+            Assert.False(isModelStateNotValid);
+            Assert.True(isModelStateValid);
+        }
+    }
+}
--- CrossSolar/Models/PanelModel.cs
+++ CrossSolar/Models/PanelModel.cs
@@ -8,9 +8,11 @@ namespace CrossSolar.Models
 
         [Required]
         [Range(-90, 90)]
-        [RegularExpression(@"^\d+(\.\d{6})$")]
+        [RegularExpression(@"^-?\d+((\.|\,)\d{1,6})?$")]
         public double Latitude { get; set; }
 
+        [Required]
+        [RegularExpression(@"^-?\d+((\.|\,)\d{1,6})?$")]
         [Range(-180, 180)] public double Longitude { get; set; }
 
         [Required] public string Serial { get; set; }
