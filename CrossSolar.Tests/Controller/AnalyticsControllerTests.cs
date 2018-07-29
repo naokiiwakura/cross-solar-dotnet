5a725099ddb2bf8884626c068bf494612b528ef2
elton naoki iwakura <naoki_iwakura@hotmail.com>
2018-07-29 12:36:16 -0400

    analitics controller test and panl repository test

---
+++ CrossSolar.Tests/Controller/AnalyticsControllerTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CrossSolar.Controllers;
+using CrossSolar.Domain;
+using CrossSolar.Models;
+using CrossSolar.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xunit;
+
+namespace CrossSolar.Tests.Controller
+{
+    public class AnaliticsControllerTests
+    {
+        public AnaliticsControllerTests()
+        {
+            _analyticsController = new AnalyticsController(_analyticsRepositoryMock.Object, _panelRepositoryMock.Object);
+        }
+
+        private readonly AnalyticsController _analyticsController;
+
+        private readonly Mock<IAnalyticsRepository> _analyticsRepositoryMock = new Mock<IAnalyticsRepository>();
+        private readonly Mock<IPanelRepository> _panelRepositoryMock = new Mock<IPanelRepository>();
+
+        [Fact]
+        public async Task ShouldInsertOneHourElectricity()
+        {
+            var panelID = "XXXX1111YYYY2222";
+            var oneHourElectricityModel = new OneHourElectricityModel
+            {
+                KiloWatt = 17
+            };
+
+            // Arrange
+
+            // Act
+            var result = await _analyticsController.Post(panelID, oneHourElectricityModel);
+
+            // Assert
+            Assert.NotNull(result);
+
+            var createdResult = result as CreatedResult;
+            Assert.NotNull(createdResult);
+            Assert.Equal(201, createdResult.StatusCode);
+        }
+
+
+
+
+        //[Fact]
+        //public async Task GetPowerPanelOutPut()
+        //{
+        //    //Arrange
+        //    var panelID = "XXXX1111YYYY2222";
+        //    _panelRepositoryMock.Setup(m => m.Query()
+        //        .FirstOrDefault(It.IsAny<Func<Panel, bool>>())).Returns(new Panel { Id = 1, Serial = "XXXX1111YYYY2222" });
+
+        //    _analyticsRepositoryMock.Setup(m => m.Query()
+        //        .Where(x => x.PanelId.Equals(panelID)).ToList()).Returns(new List<OneHourElectricity>());
+
+        //    //Act
+        //    var result = await _analyticsController.Get(panelID);
+
+        //    // Assert
+        //    Assert.NotNull(result);
+
+        //    var objectResult = result as OkObjectResult;
+        //    Assert.NotNull(objectResult);
+
+        //    var content = objectResult.Value as OneHourElectricityListModel;
+        //    Assert.NotNull(content);
+        //}
+    }
+}
---
+++ CrossSolar.Tests/Repository/PanelRepositoryTests.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using CrossSolar.Controllers;
+using CrossSolar.Domain;
+using CrossSolar.Models;
+using CrossSolar.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xunit;
+
+namespace CrossSolar.Tests.Repository
+{
+    public class PanelRepositoryTests
+    {
+        public PanelRepositoryTests()
+        {
+            _panelRepository = new PanelRepository(_crossSolarDbContextMock.Object);
+        }
+
+        private readonly PanelRepository _panelRepository;
+
+        private readonly Mock<CrossSolarDbContext> _crossSolarDbContextMock = new Mock<CrossSolarDbContext>();
+
+
+        [Fact]
+        public async Task test_get()
+        {
+
+            // Arrange
+            List<Panel> panels = new List<Panel>();
+            _crossSolarDbContextMock.Setup(m => m.FindAsync<Panel>(1)).Returns(Task.FromResult<Panel>(new Panel { Id = 1 }));
+
+            // Act
+            var result = _panelRepository.GetAsync("1");
+
+            // Assert
+            Assert.NotNull(result);
+
+
+            Assert.Equal(1, result.Id);
+        }
+
+        [Fact]
+        public async Task update_query()
+        {
+
+            // Arrange
+            List<Panel> panels = new List<Panel>();
+            _crossSolarDbContextMock.Setup(m => m.FindAsync<Panel>(1)).Returns(Task.FromResult<Panel>(new Panel { Id = 1 }));
+
+            // Act
+            var result = _panelRepository.GetAsync("1");
+
+            // Assert
+            Assert.NotNull(result);
+
+
+            Assert.Equal(1, result.Id);
+        }
+
+        [Fact]
+        public async Task insert_query()
+        {
+            var panel = new Panel
+            {
+                Brand = "Areva",
+                Latitude = 12.345678,
+                Longitude = 98.7655432,
+                Serial = "AAAA1111BBBB2222"
+            };
+
+            _crossSolarDbContextMock.Setup(m => m.Set<Panel>().Add(panel));
+
+            // Arrange
+
+            // Act
+            await _panelRepository.InsertAsync(panel);
+
+            // Assert
+
+            Assert.Equal(typeof(Panel), panel.GetType());
+        }
+
+        //[Fact]
+        //public async Task Test_query()
+        //{
+
+        //    // Arrange
+        //    List<Panel> panels = new List<Panel>();
+        //    _crossSolarDbContextMock.Setup(m => m.Set<Panel>().AsQueryable().Where(It.IsAny<Expression<Func<Panel, bool>>>())).Returns(panels.AsQueryable());       
+
+        //    // Act
+        //    var result = _panelRepository.Query();
+
+        //    // Assert
+        //    Assert.NotNull(result);
+
+        //    var createdResult = result as CreatedResult;
+        //    Assert.NotNull(createdResult);
+        //    Assert.Equal(201, createdResult.StatusCode);
+        //}
+    }
+}
